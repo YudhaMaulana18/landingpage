c2b80c574ed1bd3b9a88e634c2901893b47d39a3
YudhaMaulana18 <yudhadavino06@gmail.com>
2026-08-01 00:45:19 +0800

    update: web + APK dengan panel validasi KRS untuk admin/dosen

--- flutter/flutter_bootstrap.js
+++ flutter/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3277494819" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3488803470" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
