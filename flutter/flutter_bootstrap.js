d551f9900bb32d7ed6fc1c2198fc0e9359fc4fe4
YudhaMaulana18 <yudhadavino06@gmail.com>
2026-08-01 02:38:35 +0800

    feat: bump to 1.0.1+2 (cache bust) - rebuild web + APK with staff full navigation

--- flutter/flutter_bootstrap.js
+++ flutter/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4244481298" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3782466564" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
